d26eccc91c721394ca05684f5fd4762396b01e15
Nitya-Gupta-3-10 <nitya31007gupta@gmail.com>
2026-08-22 12:45:46 +0530

    Add mergesort function for merging two arrays
    Implement mergesort function to merge two sorted arrays.

---
+++ mergesort.py
@@ -0,0 +1,16 @@
+def mergesort(arr1,arr2,merge):
+    i=0
+    j=0
+    while i<len(arr1) and j<len(arr2):
+        if arr1[i]<arr2[j]:
+            merge.append(arr1[i])
+            i=i+1
+        else :
+            merge.append(arr2[j])
+            j=j+1
+
+arr1=[1,3,5]
+arr2=[2,4,6]
+merge=[]
+mergesort(arr1,arr2,merge)
+print(merge)
